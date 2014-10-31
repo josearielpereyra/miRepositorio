038b9632fb573712fe37477505d2695f2f5d62db
Jose Ariel Pereyra <josearielpereyra@gmail.com>
2014-10-31 07:40:08 -0400

    se ha agregado la obtencion del nombre...

--- prueba_git/src/prueba_git/PruebaPersona.java
+++ prueba_git/src/prueba_git/PruebaPersona.java
@@ -8,6 +8,8 @@ public class PruebaPersona {
 		Persona persona1 = new Persona("Juan Perez", "Calle Principal #45", "809-555-5555", 
 										new GregorianCalendar(1975, 10, 25));
 		persona1.mostrarDatos();
+		System.out.println( persona1.obtenerNombre() );
+
 	}
 
 }
